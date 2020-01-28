7f361f64ae83a0f0dcff84d9ec3744c317ab9a2c
Max Beatty <mbeatty@squareup.com>
2020-01-28 09:29:06 -0800

    feat: sample storybook

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an example repository using [theintern/intern](https://github.com/theintern/intern). Branches contain reproductions for issues found.
---
+++ stories/example.stories.js
@@ -0,0 +1,7 @@
+export default { title: "Example" };
+
+export const basic = () => `
+    <form id="basic-example">
+        <input name="name" placeholder="What is your name?" />
+    </form>
+`;
